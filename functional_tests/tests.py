b64558a8e4add7c86d1ee2fd5b383de51da614c1
Maxim Esin <mesin@micromine.com.au>
2020-04-08 13:26:01 +0300

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -50,6 +50,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates< and now the page lists
         # 1: Buy peacock feathers as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She
@@ -58,7 +59,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id("id_new_item")
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
+        time.sleep(3)
         self.wait_for_row_in_list_table("1: Buy peacock feathers")
         self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
@@ -99,7 +100,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, "/list/.+")
+        self.assertRegex(francis_list_url, "/lists/.+/")
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
